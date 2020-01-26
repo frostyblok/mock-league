88441e81667232f98bbcb3670f8d79e92fb85cdf
Oluwakunle Fakorede <oluwakunlefakorede@Oluwakunles-MacBook-Pro.local>
2020-01-26 22:50:45 +0100

    Create user model and introduce jwt

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,17 @@
+class JsonWebToken
+  HMAC_SECRET = MockLeague::Application.credentials.secret_key_base
+
+  def self.encode(payload, expires_in = 24.hours.from_now)
+    payload[:exp] = expires_in.to_i
+
+    JWT.encode(payload, HMAC_SECRET)
+  end
+
+  def self.decode(token)
+    response = JWT.decode(token, HMAC_SECRET).first
+
+    HashWithIndifferentAccess.new(response)
+
+    rescue JWT::DecodeError
+  end
+end
