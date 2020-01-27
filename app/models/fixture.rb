58731b6be5059526630c9c900977fec8ba3e4c9f
Oluwakunle Fakorede <oluwakunlefakorede@Oluwakunles-MacBook-Pro.local>
2020-01-27 22:55:07 +0100

    Add date field to fixtures

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -1,5 +1,5 @@
 class Fixture < ApplicationRecord
-  validates_presence_of :home_team_id, :away_team_id
+  validates_presence_of :home_team_id, :away_team_id, :date
   belongs_to :home_team, class_name: "Team", foreign_key: :home_team_id
   belongs_to :away_team, class_name: "Team", foreign_key: :away_team_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_144415) do
+ActiveRecord::Schema.define(version: 2020_01_27_213605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_01_27_144415) do
     t.integer "away_team_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "date"
   end
 
   create_table "teams", force: :cascade do |t|
