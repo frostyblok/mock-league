539bafe6cd1d55adb345fd7ccee407475c227292
Oluwakunle Fakorede <oluwakunlefakorede@Oluwakunles-MacBook-Pro.local>
2020-01-30 10:59:53 +0100

    validate the inclusion of completed and role respectively in fixtures and users

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -2,6 +2,8 @@
 
 class Fixture < ApplicationRecord
   validates_presence_of :home_team_id, :away_team_id, :date
+  validates_inclusion_of :completed, in: %w[done pending]
+
   belongs_to :home_team, class_name: 'Team', foreign_key: :home_team_id
   belongs_to :away_team, class_name: 'Team', foreign_key: :away_team_id
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:password_digest) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
+
+  context 'validates presence of role' do
+    let(:user) { create(:user, role: 'guest') }
+
+    it 'raises a record invalid error' do
+      expect { user }
+        .to raise_error(
+              ActiveRecord::RecordInvalid,
+              /Validation failed: Role is not included in the list/
+            )
+    end
+  end
+
 end
